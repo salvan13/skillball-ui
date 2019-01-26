59e9c6aa7f241a0659bdef7c77a7d22d382e67b9
Antonio Salvati <antonio.salvati@skillbill.it>
2019-01-26 15:34:23 +0100

    fixed array thing

--- src/libs/game.js
+++ src/libs/game.js
@@ -10,7 +10,10 @@ export default class Game {
         goalkeeper: 0,
         striker: 0
       },
-      moves: [],
+      moves: {
+        p0: null,
+        p1: null
+      },
       animation: {
         active: false,
         goalkeeper: '',
@@ -56,26 +59,28 @@ export default class Game {
   }
 
   onMessage(msg) {
-    if(!this.state.moves[msg.player]) {
-      this.state.moves[msg.player] = msg;
+    if(!this.state.moves[`p${msg.player}`]) {
+      this.state.moves[`p${msg.player}`] = msg;
     }
-    if(this.state.moves[0] && this.state.moves[1]) {
-      if(this.state.moves[0].move === this.state.moves[1].move) {
+    if(this.state.moves.p0 && this.state.moves.p1) {
+      if(this.state.moves.p0.move === this.state.moves.p1.move) {
         this.state.score.goalkeeper++;
       } else {
         this.state.score.striker++;
       }
       this.state.animation = {
         active: true,
-        goalkeeper: this.state.moves[0].move,
-        ball: this.state.moves[1].move,
-        goal: this.state.moves[0].move !== this.state.moves[1].move ? '1' : ''
+        goalkeeper: this.state.moves.p0.move,
+        ball: this.state.moves.p1.move,
+        goal: this.state.moves.p0.move !== this.state.moves.p1.move ? '1' : ''
       }
+
+      setTimeout(() => {
+        this.nextTurn();
+      }, 5000);
+
     }
 
-    setTimeout(() => {
-      this.nextTurn();
-    }, 5000);
   }
 
   nextTurn() {
